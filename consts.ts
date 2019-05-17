50f3990d4668e7082d4c3492707d393ba9413696
alk831 <adam54111@gmail.com>
2019-05-17 13:54:05 +0200

    add more action type consts

--- consts.ts
+++ consts.ts
@@ -1,4 +1,23 @@
 
+/* GAME */
+export const $_LOAD_GAME = '$_LOAD_GAME';
+export const LOAD_LOCATION = 'LOAD_LOCATION';
+export const UPDATE_CHARACTER = 'UPDATE_CHARACTER';
+export const CHARACTER_UPDATE = 'CHARACTER_UPDATE';
+export const CHARACTER_LEAVE = 'CHARACTER_LEAVE';
+export const CHARACTER_JOIN = 'CHARACTER_JOIN';
+export const CHANGE_LOCATION = 'CHANGE_LOCATION';
+export const REQUEST_LOCATION_CHANGE = 'REQUEST_LOCATION_CHANGE';
+export const MOUSE_POSITION_UPDATE = 'MOUSE_POSITION_UPDATE';
+
+
+/* LOCATION */
+export const $_CHARACTER_UPDATE = '$_CHARACTER_UPDATE';
+export const $_CHARACTER_JOIN = '$_CHARACTER_JOIN';
+export const $_CHARACTER_LEAVE = '$_CHARACTER_LEAVE';
+
+
+/* NPC */
 export const NPC_DIALOG_REQUEST = 'NPC_DIALOG_REQUEST';
 export const NPC_DIALOG_CLOSE = 'NPC_DIALOG_CLOSE';
 export const $_NPC_DIALOG_RESPONSE = '$_NPC_DIALOG_RESPONSE';
@@ -8,3 +27,28 @@ export const $_NPC_SHOP_RESPONSE = '$_NPC_SHOP_RESPONSE';
 
 export const NPC_SHOP_TRADE = 'NPC_SHOP_TRADE';
 export const $_NPC_SHOP_TRADE = '$_NPC_SHOP_TRADE';
+
+
+/* CHAT */
+export const SEND_MESSAGE = 'SEND_MESSAGE';
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+
+
+/* INVENTORY */
+export const ITEM_ADD = 'ITEM_ADD';
+export const ITEM_UPDATE = 'ITEM_UPDATE';
+export const ITEM_REMOVE = 'ITEM_REMOVE';
+
+export const ITEM_PICKUP = 'ITEM_PICKUP';
+export const ITEM_DROP = 'ITEM_DROP';
+
+export const $_ITEM_DROPPED_ADD = '$_ITEM_DROPPED_ADD';
+export const $_ITEM_DROPPED_REMOVE = '$_ITEM_DROPPED_REMOVE';
+
+
+/* BATTLE */
+export const FIGHT_START = 'FIGHT_START';
+export const FIGHT_ACTION = 'FIGHT_ACTION';
+
+export const $_FIGHT_ACTION_RESULT = '$_FIGHT_ACTION_RESULT';
+export const $_FIGHT_FINISH = '$_FIGHT_FINISH';
